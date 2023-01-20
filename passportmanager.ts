a558307ac6ac395459b7f474f484cc38ff88291b
TheJeterLP <joey.peter1998@gmail.com>
2023-01-20 09:30:37 +0100

    add google oauth2 authentication

--- passportmanager.ts
+++ passportmanager.ts
@@ -37,7 +37,7 @@ export function setupPassport(app: Application) {
     ));
 
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-    app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/error' }),
+    app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/auth/error' }),
         function (req, res) {
             // Successful authentication, redirect success.
             res.redirect('/auth/success');
