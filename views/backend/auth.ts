4ce8b616403be0f3500be55f080227507891161a
TheJeterLP <joey.peter1998@gmail.com>
2023-01-21 00:40:53 +0100

    dont show auth page when logged in

--- views/backend/auth.ts
+++ views/backend/auth.ts
@@ -1,3 +1,14 @@
+import { Request, Response } from 'express';
+import { isLoggedIn } from '../../controllers/auth.controller';
+
 export const title = 'Authenticate';
 export const pugfile = 'auth.pug';
 export const urlpath = '/auth';
+
+export async function onLoad(req: Request, res: Response): Promise<Map<string, any>> {
+    if (await isLoggedIn(req)) {
+        res.status(403).send('Already logged in');
+    }
+    const map = new Map<string, any>();
+    return map;
+}
