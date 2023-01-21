e8b84b91d026cacb2cc39a3234d2e1d0d3ace1d7
TheJeterLP <joey.peter1998@gmail.com>
2023-01-21 19:56:45 +0100

    Add signout function

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Request, Response } from 'express';
+import { Request } from 'express';
 
 export async function checkIfUserExists(emailVal: string): Promise<boolean> {
     const res = await axios.post('https://api.expenseman.app/auth/checkuser', JSON.stringify({ email: emailVal }), {
@@ -21,11 +21,4 @@ export function isLoggedIn(req: Request): boolean {
     } else {
         return false;
     }
-}
-
-export function logOut(res: Response) {
-    res.clearCookie('userId');
-    res.clearCookie('accessToken');
-
-    res.status(200).send({ message: 'You\'ve been logged out.' });
 }
