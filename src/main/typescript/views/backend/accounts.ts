0bb64173271ab9bffe1a750a4b86f72c691b5f9f
TheJeterLP <joey.peter1998@gmail.com>
2023-03-01 19:08:50 +0100

    Start with accounts page

--- src/main/typescript/views/backend/accounts.ts
+++ src/main/typescript/views/backend/accounts.ts
@@ -26,7 +26,18 @@ export async function onLoad(req: Request, res: Response): Promise<Map<string, s
         },
     });
 
-    debug(response.data);
+    if (response.status != 200) {
+        let msg = `Error ${response.status} An unknown error happened.`;
+        if (response.data.message) {
+            msg = `Error ${response.status}: ${response.data.message}`;
+        }
+        res.cookie('errormsg', msg);
+        res.status(response.status).redirect('/error');
+    } else {
+        // Status 200
+        debug(response.data);
+    }
+    
     const map = new Map<string, string>();
     return map;
 }
