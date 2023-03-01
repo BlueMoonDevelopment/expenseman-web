6601ebd5c0929cbd2405bfe611a649f6b675b383
TheJeterLP <joey.peter1998@gmail.com>
2023-03-01 20:10:13 +0100

    Start with accounts page

--- src/main/typescript/tools/routemanager.ts
+++ src/main/typescript/tools/routemanager.ts
@@ -103,7 +103,7 @@ export function loadRoutes(app: Application) {
                         if (!value || key == 'msg') continue;
                         values.set(key, value);
                     }
-                    options['values'] = values.values();
+                    options['values'] = Array.from(values.values());
                 }
 
                 if (variables.size > 0) {
